6a978a019f67fb0b972973f446c4f905bdd43d07
Omar Othman <omar.othman@live.com>
2020-08-29 10:28:49 +0300

    Correct util test name

--- test/utilTest.js
+++ test/utilTest.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const util = require('../src/util');
 
-describe('Util', () => {
+describe('util', () => {
   describe('#isString()', () => {
     it('should return true when the value is a literal string', () => {
       assert.equal(util.isString('literal string'), true);
